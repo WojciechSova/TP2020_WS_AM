5d3bd2a2adb2e5b755ade779af5a052f9bd234ff
WojciechSova <dzidadzidowski@gmail.com>
2020-11-11 18:12:42 +0100

    Add DataRepository interface

--- Task1/Task1/Data/IDataRepository.cs
+++ Task1/Task1/Data/IDataRepository.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace Task1.Data
 {
-    class IDataRepository
+    public interface IDataRepository
     {
+        #region Reader
+        void AddReader(Reader reader);
+        Reader GetReader(int id);
+        IEnumerable<Reader> GetAllReaders();
+        void UpdateReader(int id, string name, string surname, long personalID);
+        void DeleteReader(Reader reader);
+        #endregion
+
+        #region Book
+        void AddBook(Book book);
+        Book GetBook(int id);
+        IEnumerable<Book> GetAllBook();
+        void UpdateBook(int id, string isbn, string author, string title, string description);
+        void DeleteBook(Book book);
+        #endregion
+
+        #region BookState
+        void AddBookState(BookState bookState);
+        BookState GetBookState(int id);
+        IEnumerable<BookState> GetAllBookState();
+        void UpdateBookState(int id, Book book, bool state);
+        void DeleteBookState(BookState bookState);
+        #endregion
+
+        #region BookEvent
+        void AddEvent(BookEvent bookEvent);
+        BookEvent GetBookEvent(int id);
+        IEnumerable<BookEvent> GetAllBookEvent();
+        void UpdateBookEvent(int id, Reader reader, BookState bookState);
+        void DeleteBookEvent(BookEvent bookEvent);
+        #endregion
     }
 }
