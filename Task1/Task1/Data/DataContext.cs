fe0ebb1104d9521e50d0db93e729492f01d909c2
Artur <artur.m99@wp.pl>
2020-11-12 21:48:25 +0100

    DataContext class is implemented and ready to use

--- Task1/Task1/Data/DataContext.cs
+++ Task1/Task1/Data/DataContext.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Task1.Data;
 
 namespace Task1
 {
     public class DataContext
     {
+        List<Reader> ReadersList { get; set; }
+        Dictionary<string, BookState> BookSet{get; set;}
+        ObservableCollection<BookEvent> BookEvents { get; set; }
+        List<BookState> BookStatesList { get; set; }
+
+        public DataContext()
+        {
+            ReadersList = new List<Reader>();
+            BookSet = new Dictionary<string, BookState>();
+            BookEvents = new ObservableCollection<BookEvent>();
+            BookStatesList = new List<BookState>();
+        }
     }
 }
