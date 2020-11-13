f806a25c73a13b00280de2f5990ffa2369de87b6
Artur <artur.m99@wp.pl>
2020-11-13 18:39:14 +0100

    error fixed - there is field BookState now  instead of Book

--- Task1/Task1/Data/ObjectModel/BookEvents/BookEvent.cs
+++ Task1/Task1/Data/ObjectModel/BookEvents/BookEvent.cs
@@ -5,18 +5,18 @@ namespace Task1.Data
     public abstract class BookEvent
     {
         public Reader Reader { get; set; }
-        public Book Book { get; set; }
+        public BookState BookState { get; set; }
         public DateTime EventTime { get; }
 
         
-        protected BookEvent(Reader reader, Book book, DateTime dateTime)
+        protected BookEvent(Reader reader, BookState bookState, DateTime dateTime)
         {
             Reader = reader;
-            Book = book;
+            BookState = bookState;
             EventTime = dateTime;
         }
 
-        protected BookEvent(Reader reader, Book book) : this(reader, book, DateTime.Now)
+        protected BookEvent(Reader reader, BookState bookState) : this(reader, bookState, DateTime.Now)
         {
 
         }
