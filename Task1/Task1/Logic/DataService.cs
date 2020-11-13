1454d768666ea5b5e6f9be7cc294576540d31d2a
WojciechSova <dzidadzidowski@gmail.com>
2020-11-13 18:57:56 +0100

    Implement Adds methods in DataService Add Book constructor

--- Task1/Task1/Logic/DataService.cs
+++ Task1/Task1/Logic/DataService.cs
@@ -1,9 +1,9 @@
 using System;
 using Task1.Data;
 
-namespace Task1
+namespace Task1.Logic
 {
-    public class DataService implements IDataService
+    public class DataService : IDataService
     {
         private IDataRepository IDataRepository;
 
@@ -12,7 +12,37 @@ namespace Task1
             IDataRepository = iDataRepository;
         }
 
+        public void AddBook(string Isbn, string Author, string Title, string Description)
+        {
+            Book book = new Book(Isbn, Author, Title, Description);
+            IDataRepository.AddBook(book);
+        }
 
+        public void AddReader(string Name, string Surname, long PersonalId)
+        {
+            Reader reader = new Reader(Name, Surname, PersonalId);
+            IDataRepository.AddReader(reader);
+        }
 
+        public void AddBookState(Book Book, bool Available, DateTime BuyingTime)
+        {
+            BookState bookState = new BookState(Book, Available, BuyingTime);
+            IDataRepository.AddBookState(bookState);
+        }
+
+        public void DeleteBook(string Isbn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteReader(long PersonalId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBookState(string isbn)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
