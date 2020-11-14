880dfcd2f79b66d08e5420ea4fb651a18aa2b142
WojciechSova <dzidadzidowski@gmail.com>
2020-11-14 13:08:18 +0100

    Add regions in DataRepository Change BookSet to Dictionary<int, Book>

--- Task1/Task1/Data/DataRepository.cs
+++ Task1/Task1/Data/DataRepository.cs
@@ -13,27 +13,7 @@ namespace Task1.Data
             DataContext = new DataContext();
         }
 
-        public void AddBook(Book book)
-        {
-            if(DataContext.BookSet.ContainsKey(book.Isbn))
-            {
-                throw new ArgumentException("Book with provided isbn already exists");
-            }
-
-            
-            DataContext.BookSet.Add(book.Isbn, book.Title);
-        }
-
-        public void AddBookState(BookState bookState)
-        {
-            DataContext.BookStatesList.Add(bookState);
-        }
-
-        public void AddEvent(BookEvent bookEvent)
-        {
-            DataContext.BookEvents.Add(bookEvent);
-        }
-
+        #region Reader
         public void AddReader(Reader reader)
         {
             if (DataContext.ReadersList.Contains(reader))
@@ -42,6 +22,73 @@ namespace Task1.Data
             }
 
             DataContext.ReadersList.Add(reader);
+        }
+
+        public Reader GetReader(int id)
+        {
+            if (DataContext.ReadersList.ElementAtOrDefault(id) != null)
+            {
+                return DataContext.ReadersList[id];
+            }
+            throw new KeyNotFoundException("Reader id: " + id + " do not exist");
+        }
+        public IEnumerable<Reader> GetAllReaders()
+        {
+            return DataContext.ReadersList;
+        }
+        public void UpdateReader(int id, string name, string surname, long personalID)
+        {
+            if (DataContext.ReadersList.ElementAtOrDefault(id) != null)
+            {
+                DataContext.ReadersList[id].Name = name;
+                DataContext.ReadersList[id].Surname = surname;
+                DataContext.ReadersList[id].PersonalID = personalID;
+                return;
+            }
+            throw new KeyNotFoundException("Reader id: " + id + " do not exist");
+        }
+
+        public void DeleteReader(Reader reader)
+        {
+            foreach (BookEvent b in DataContext.BookEvents)
+            {
+                if (b.Reader == reader)
+                {
+                    throw new ArgumentException("Cannot remove client who still has a borrowed book.");
+                }
+            }
+            if (DataContext.ReadersList.Contains(reader))
+            {
+                DataContext.ReadersList.Remove(reader);
+            }
+        }
+        #endregion
+
+        #region Book
+        public void AddBook(Book book)
+        {
+            if (DataContext.BookSet.ContainsValue(book))
+            {
+                throw new ArgumentException("Book already exists");
+            }
+
+
+            DataContext.BookSet.Add(DataContext.BookSet.Count, book);
+        }
+
+        public Book GetBook(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Book> GetAllBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBook(int id, string isbn, string author, string title, string description)
+        {
+            throw new NotImplementedException();
         }
 
         public void DeleteBook(Book book)
@@ -61,68 +108,13 @@ namespace Task1.Data
             }
         }
 
-        public void DeleteBookEvent(BookEvent bookEvent)
-        {
-            if (DataContext.BookEvents.Contains(bookEvent))
-            {
-                DataContext.BookEvents.Remove(bookEvent);
-            }
-        }
+        #endregion
 
-        public void DeleteBookState(BookState bookState)
+        #region BookState
+        public void AddBookState(BookState bookState)
         {
-            throw new NotImplementedException();
+            DataContext.BookStatesList.Add(bookState);
         }
-
-        public void DeleteReader(Reader reader)
-        {
-            foreach (BookEvent b in DataContext.BookEvents)
-            {
-                if (b.Reader == reader)
-                {
-                    throw new ArgumentException("Cannot remove client who still has a borrowed book.");
-                }
-            }
-            if (DataContext.ReadersList.Contains(reader))
-            {
-                DataContext.ReadersList.Remove(reader);
-            }
-        }
-
-        public IEnumerable<Book> GetAllBook()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<BookEvent> GetAllBookEvent()
-        {
-            return DataContext.BookEvents;
-        }
-
-        public IEnumerable<BookState> GetAllBookState()
-        {
-            return DataContext.BookStatesList;
-        }
-
-        public IEnumerable<Reader> GetAllReaders()
-        {
-            return DataContext.ReadersList;
-        }
-
-        public Book GetBook(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public BookEvent GetBookEvent(int id)
-        {
-            if(DataContext.BookEvents.ElementAtOrDefault(id) != null)
-            {
-                return DataContext.BookEvents[id];
-            }
-            throw new KeyNotFoundException("Event id: " + id + " do not exist");
-        }
-
         public BookState GetBookState(int id)
         {
             if (DataContext.BookStatesList.ElementAtOrDefault(id) != null)
@@ -131,33 +123,10 @@ namespace Task1.Data
             }
             throw new KeyNotFoundException("State id: " + id + " do not exist");
         }
-
-        public Reader GetReader(int id)
+        public IEnumerable<BookState> GetAllBookState()
         {
-            if (DataContext.ReadersList.ElementAtOrDefault(id) != null)
-            {
-                return DataContext.ReadersList[id];
-            }
-            throw new KeyNotFoundException("Reader id: " + id + " do not exist");
+            return DataContext.BookStatesList;
         }
-
-        public void UpdateBook(int id, string isbn, string author, string title, string description)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateBookEvent(int id, Reader reader, BookState bookState, DateTime dateTime)
-        {
-            if (DataContext.BookEvents.ElementAtOrDefault(id) != null)
-            {
-                DataContext.BookEvents[id].Reader = reader;
-                DataContext.BookEvents[id].BookState = bookState;
-                DataContext.BookEvents[id].EventTime = dateTime;
-                return;
-            }
-            throw new KeyNotFoundException("Event id: " + id + " do not exist");
-        }
-
         public void UpdateBookState(int id, Book book, bool available, DateTime buyingDate)
         {
             if (DataContext.BookStatesList.ElementAtOrDefault(id) != null)
@@ -169,17 +138,47 @@ namespace Task1.Data
             }
             throw new KeyNotFoundException("State id: " + id + " do not exist");
         }
-
-        public void UpdateReader(int id, string name, string surname, long personalID)
+        public void DeleteBookState(BookState bookState)
         {
-            if (DataContext.ReadersList.ElementAtOrDefault(id) != null)
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region BookEvent
+        public void AddEvent(BookEvent bookEvent)
+        {
+            DataContext.BookEvents.Add(bookEvent);
+        }
+        public BookEvent GetBookEvent(int id)
+        {
+            if (DataContext.BookEvents.ElementAtOrDefault(id) != null)
             {
-                DataContext.ReadersList[id].Name = name;
-                DataContext.ReadersList[id].Surname = surname;
-                DataContext.ReadersList[id].PersonalID = personalID;
+                return DataContext.BookEvents[id];
+            }
+            throw new KeyNotFoundException("Event id: " + id + " do not exist");
+        }
+        public IEnumerable<BookEvent> GetAllBookEvent()
+        {
+            return DataContext.BookEvents;
+        }
+        public void UpdateBookEvent(int id, Reader reader, BookState bookState, DateTime dateTime)
+        {
+            if (DataContext.BookEvents.ElementAtOrDefault(id) != null)
+            {
+                DataContext.BookEvents[id].Reader = reader;
+                DataContext.BookEvents[id].BookState = bookState;
+                DataContext.BookEvents[id].EventTime = dateTime;
                 return;
             }
-            throw new KeyNotFoundException("Reader id: " + id + " do not exist");
+            throw new KeyNotFoundException("Event id: " + id + " do not exist");
         }
+        public void DeleteBookEvent(BookEvent bookEvent)
+        {
+            if (DataContext.BookEvents.Contains(bookEvent))
+            {
+                DataContext.BookEvents.Remove(bookEvent);
+            }
+        }
+        #endregion
     }
 }
