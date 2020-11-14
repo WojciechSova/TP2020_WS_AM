a5e1f603d7f35f1d7e2276db2c33d4625af2c131
Artur <artur.m99@wp.pl>
2020-11-14 18:44:02 +0100

    Data Filler interface implemented, constant filler is ready to use

---
+++ Task1/Task1Test/DataFiller/RandomFiller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Task1.Data;
+
+namespace Task1Test.DataFiller
+{
+    class RandomFiller : IDataFiller
+    {
+        public void Fill(DataContext dataContext)
+        {
+          
+        }
+    }
+}
