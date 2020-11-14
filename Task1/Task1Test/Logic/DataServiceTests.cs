5626e844247c6894971a9c560ac20dd04544f1a8
Artur <artur.m99@wp.pl>
2020-11-14 23:23:49 +0100

    Started to implement tests. ConstantFiller is tested. Everything works fine so far.

---
+++ Task1/Task1Test/Logic/DataServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1.Data;
+using Task1.Logic;
+using Task1Test.DataFiller;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task1Test.Logic
+{
+    [TestClass]
+    public class DataServiceTests
+    {
+        private IDataFiller dataFiller = new ConstantFiller();
+        private DataContext dataContext = new DataContext();
+        private IDataRepository dataRepository;
+        private IDataService dataService;
+
+        [TestMethod]
+        public void RentBookTest()
+        {
+            IDataRepository dataRepository = new DataRepository(dataFiller, dataContext);
+            dataService = new DataService(dataRepository);
+
+
+        }
+    }
+}
