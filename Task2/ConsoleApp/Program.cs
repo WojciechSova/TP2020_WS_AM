0d10bcf4503a6f1cee4ccc61443ebee3dce97c06
Artur <artur.m99@wp.pl>
2020-12-06 22:56:17 +0100

    Correct program

--- Task2/ConsoleApp/Program.cs
+++ Task2/ConsoleApp/Program.cs
@@ -8,41 +8,28 @@ namespace ConsoleApp
 {
     class Program
     {
-        static String filePath = "..\\..\\..\\..\\TestResults\\CustomSerialization.saved";
         static void Main(string[] args)
         {
             DataContext dataContext = new DataContext();
             IDataFiller dataFiller = new ConstantFiller();
             IDataRepository dataRepository;
-            dataRepository = new Task2.Data.DataRepository(dataFiller, dataContext);
+            dataRepository = new DataRepository(dataFiller, dataContext);
             dataContext.countEvents();
             CustomFormatter customFormatter = new CustomFormatter();    
 
-            using (Stream  stream = new FileStream(filePath, FileMode.Create))
-            {
-                customFormatter.Serialize(stream, dataContext);
-            }
 
-            DataContext dataContext2;
-
-            using (Stream stream = new FileStream(filePath, FileMode.Open))
-            {
-                dataContext2 = (DataContext)customFormatter.Deserialize(stream);
-            }
-
-           /* Console.WriteLine("Wciśnij:");
+            Console.WriteLine("Wciśnij:");
             Console.WriteLine("1 - by zapisać stan programu w pliku JSON");
             Console.WriteLine("2 - by zapisać stan programu własną metodą");
             Console.WriteLine("3 - by wczytać stan programu z pliku JSON");
             Console.WriteLine("4 - by wczytać stan programu własną metodą");
             Console.WriteLine("5 - by wyświetlić wszystkie dane");
             Console.WriteLine("6 - by wyłączyć program");
-            String option = "0";
-            String filePath;
+            string option = "0";
+            string filePath;
             while (option != "6")
             {
                 option = Console.ReadLine();
-                Console.WriteLine(option);
                 switch (option)
                 {
                     case "1":
@@ -50,18 +37,24 @@ namespace ConsoleApp
                         JsonSerializer.Serialize(dataContext, filePath);
                         break;
                     case "2":
-
+                        filePath = "..\\..\\..\\..\\TestResults\\CustomSerialization.saved";
+                        using (Stream stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            customFormatter.Serialize(stream, dataContext);
+                        }
                         break;
                     case "3":
                         filePath = "..\\..\\..\\..\\TestResults\\jsonFile.json";
-                        Console.WriteLine("aaaa3");
                         dataContext = JsonSerializer.Deserialize<DataContext>(filePath);
-                        dataRepository = new DataRepository(dataContext);
-                        Console.WriteLine(dataRepository.GetAllReaders().ToString());
                         break;
                     case "4":
+                        filePath = "..\\..\\..\\..\\TestResults\\CustomSerialization.saved";
+                        using (Stream stream = new FileStream(filePath, FileMode.Open))
+                        {
+                            dataContext = (DataContext)customFormatter.Deserialize(stream);
+                        }
                         break;
-                    case "5":*/
+                    case "5":
                         dataContext.ReadersList.ForEach(i => Console.WriteLine(i.ToString()));
                         foreach (KeyValuePair<int, Book> temp in dataContext.BookSet)
                         {
@@ -72,11 +65,11 @@ namespace ConsoleApp
                         {
                             Console.WriteLine(temp.ToString());
                         }
-                       /* break;
+                        break;
                     default:
                         break;
                 }
-            }*/
+            }
 
 
         }
