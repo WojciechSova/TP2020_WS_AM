90038d31fde1f5734bf398a8337589be015b0318
Artur <artur.m99@wp.pl>
2020-12-06 15:14:07 +0100

    Move serializers to Data

---
+++ Task2/Task2/Data/CustomFormatter.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Task2.Data
+{
+    public class CustomFormatter : Formatter
+    {
+        private List<XElement> values = new List<XElement>();
+        public override void Serialize(Stream serializationStream, object graph)
+        {
+            ISerializable serializable = (ISerializable)graph;
+            SerializationInfo serializationInfo = new SerializationInfo(graph.GetType(), new FormatterConverter());
+            StreamingContext streamingContext = new StreamingContext(StreamingContextStates.File);
+            serializable.GetObjectData(serializationInfo, streamingContext);
+            foreach (SerializationEntry item in serializationInfo)
+            {
+                this.WriteMember(item.Name, item.Value);
+            }
+
+            using (StreamWriter writer = new StreamWriter(serializationStream))
+            {
+                foreach (XElement element in values)
+                {
+                    writer.Write($"{element.Name} {element.Value}\n");
+                }
+            }
+        }
+
+        public override object Deserialize(Stream serializationStream)
+        {
+            using (StreamReader reader = new StreamReader(serializationStream))
+            {
+                foreach (XElement element in values)
+                {
+                    
+                }
+            }
+
+            return new DataContext();
+        }
+
+
+
+        public override SerializationBinder Binder { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override StreamingContext Context { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override ISurrogateSelector SurrogateSelector { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        protected override void WriteArray(object obj, string name, Type memberType)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteBoolean(bool val, string name)
+        {
+            values.Add(new XElement(name, (object) val));
+        }
+
+        protected override void WriteByte(byte val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteChar(char val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteDateTime(DateTime val, string name)
+        {
+            values.Add(new XElement(name, val));
+        }
+
+        protected override void WriteDecimal(decimal val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteDouble(double val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteInt16(short val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteInt32(int val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteInt64(long val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteObjectRef(object obj, string name, Type memberType)
+        {
+            values.Add(new XElement(name, obj.ToString()));
+        }
+
+        protected override void WriteSByte(sbyte val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteSingle(float val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteTimeSpan(TimeSpan val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteUInt16(ushort val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteUInt32(uint val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteUInt64(ulong val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteValueType(object obj, string name, Type memberType)
+        {
+            values.Add(new XElement(name, obj.ToString()));
+        }
+    }
+
+}
---
+++ Task2/Tests/Serializers/CustomSerializerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Task2.Data;
+using Tests.DataFiller;
+
+namespace Tests.Serializers
+{
+    [TestClass]
+    public class CustomSerializerTests
+    {
+        private readonly String filePath = "..\\..\\..\\..\\TestResults\\CustomSerialization.saved";
+
+        [TestMethod]
+        public void CustomSerializeTest()
+        {
+            DataContext dataContext2;
+            DataContext dataContext = new DataContext();
+            IDataFiller dataFiller = new ConstantFiller();
+            IDataRepository dataRepository;
+            dataRepository = new Task2.Data.DataRepository(dataFiller, dataContext);
+            CustomFormatter customFormatter = new CustomFormatter();
+
+            using (Stream stream = new FileStream(filePath, FileMode.Create))
+            {
+                customFormatter.Serialize(stream, dataContext);
+            }
+
+            using (Stream stream = new FileStream(filePath, FileMode.Create))
+            {
+                dataContext2 = (DataContext)customFormatter.Deserialize(stream);
+            }
+
+            Assert.AreEqual(dataContext.BookEvents[1], dataContext2.BookEvents[1]);
+        }
+    }
+}
