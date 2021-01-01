2db51e23b335920d6e95c3975d55a5b79fdb8ca6
WojciechSova <dzidadzidowski@gmail.com>
2021-01-01 16:29:34 +0100

    Added extensions methods

---
+++ Task3/Task3/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public static class Extensions
+    {
+        public static List<Product> WithoutCategory(this List<Product> lista)
+        {
+            List<Product> withoutCategory = new List<Product>();
+
+            foreach (Product P in lista)
+            {
+                if (P.ProductSubcategoryID == null) withoutCategory.Add(P);
+            }
+
+            return withoutCategory;
+        }
+
+        public static List<Product> SplitIntoPages(this List<Product> list, int size, int page)
+        {
+            List<Product> newPage = new List<Product>();
+
+            for (int i = 0; i < list.Count(); i++)
+            {
+                if (i >= size * page && i < (size * page) + size) newPage.Add(list[i]);
+            }
+
+            return newPage;
+        }
+
+    }
+}
