c37d9a800d5bafc25e523085dfae5b1dfe83e92e
WojciechSova <dzidadzidowski@gmail.com>
2021-01-03 18:16:53 +0100

    Added readme and change extensions methods

--- Task3/Task3/Extensions.cs
+++ Task3/Task3/Extensions.cs
@@ -6,31 +6,18 @@ namespace Task3
 {
     public static class Extensions
     {
-        public static List<Product> WithoutCategory(this List<Product> lista)
+        public static List<Product> WithoutCategory(this List<Product> list)
         {
-            List<Product> withoutCategory = new List<Product>();
+            IEnumerable<Product> withoutCategory = list.Where(p => p.ProductSubcategoryID == null);
 
-            foreach (Product P in lista)
-            {
-                if (P.ProductSubcategoryID == null) withoutCategory.Add(P);
-            }
-
-            return withoutCategory;
+            return withoutCategory.ToList();
         }
 
         public static List<Product> SplitIntoPages(this List<Product> list, int size, int page)
         {
-            List<Product> newPage = new List<Product>();
+            IEnumerable<Product> newPage = list.Skip(page * size).Take(size).ToList();
 
-            for (int i = 0; i < list.Count(); i++)
-            {
-                if (i >= size * page && i < (size * page) + size) 
-                { 
-                    newPage.Add(list[i]); 
-                }
-            }
-
-            return newPage;
+            return newPage.ToList();
         }
 
         public static String GetProductAndName(this List<Product> list)
@@ -43,15 +30,15 @@ namespace Task3
                 var query = new
                 {
 
-                    name =  (from p in db.Products
-                             join pv in db.ProductVendors on p.ProductID equals pv.ProductID
+                    name =  (from p in list
+                             join pv in product.ProductVendors on p.ProductID equals pv.ProductID
                              join v in db.Vendors on pv.BusinessEntityID equals v.BusinessEntityID
                              where product.ProductID == p.ProductID
                              select p.Name)
                              .FirstOrDefault(),
 
-                    vendor = (from p in db.Products
-                              join pv in db.ProductVendors on p.ProductID equals pv.ProductID
+                    vendor = (from p in list
+                              join pv in product.ProductVendors on p.ProductID equals pv.ProductID
                               join v in db.Vendors on pv.BusinessEntityID equals v.BusinessEntityID
                               where product.ProductID == p.ProductID
                               select v.Name)
