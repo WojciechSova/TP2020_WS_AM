4e1538afcffc65ca72a9644fd909dec05b386eb7
Artur <artur.m99@wp.pl>
2021-01-17 22:33:33 +0100

    Fix methods

--- Task3/Task3/ToolsMyProduct.cs
+++ Task3/Task3/ToolsMyProduct.cs
@@ -20,34 +20,32 @@ namespace Task3
 
         public List<MyProduct> MyProductGetProductsByName(string namePart)
         {
-            List<MyProduct> query = (from p in db.MyProduct
-                                     where SqlMethods.Like(p.Name, "%" + namePart + "%")
-                                     select p)
-                                     .ToList();
+            IEnumerable<MyProduct> query = (from p in db.MyProduct
+                                            where SqlMethods.Like(p.Name, "%" + namePart + "%")
+                                            select p);
 
-            return query;
+            return query.ToList();
         }
 
         public List<MyProduct> MyProductGetProductsByVendorName(string vendorName)
         {
-            List<MyProduct> query = (from p in db.MyProduct
+            IEnumerable<MyProduct> query = (from p in db.MyProduct
                                      join pv in db.ProductVendors on p.ProductID equals pv.ProductID
                                      join v in db.Vendors on pv.BusinessEntityID equals v.BusinessEntityID
                                      where SqlMethods.Like(v.Name, vendorName)
-                                     select p).ToList();
+                                     select p);
 
-            return query;
+            return query.ToList();
         }
 
         public List<MyProduct> MyProductGetNRecentlyReviewedProducts(int howManyProducts)
         {
-            List<MyProduct> query = (from p in db.MyProduct
+            IEnumerable<MyProduct> query = (from p in db.MyProduct
                                      join pr in db.ProductReview on p.ProductID equals pr.ProductID
                                      orderby pr.ReviewDate
-                                     select p)
-                                     .Take(howManyProducts).ToList<MyProduct>();
+                                     select p).Take(howManyProducts);
 
-            return query;
+            return query.ToList();
         }
     }
 }
--- Task3/Task3/ToolsProduct.cs
+++ Task3/Task3/ToolsProduct.cs
@@ -20,34 +20,33 @@ namespace Task3
 
         public List<Product> GetProductsByName(string namePart)
         {
-            List<Product> query = (from p in db.Products
-                    where SqlMethods.Like(p.Name, "%" + namePart + "%")
-                    select p)
-                    .ToList();
+            IEnumerable<Product> query = (from p in db.Products
+                                          where SqlMethods.Like(p.Name, "%" + namePart + "%")
+                                          select p);
 
-            return query;
+            return query.ToList(); ;
         }
 
         public List<Product> GetProductsByVendorName(string vendorName)
         {
-            List<Product> query = (from p in db.Products
-                                   join pv in db.ProductVendors on p.ProductID equals pv.ProductID
-                                   join v in db.Vendors on pv.BusinessEntityID equals v.BusinessEntityID
-                                   where SqlMethods.Like(v.Name, vendorName)
-                                   select p).ToList();
+            IEnumerable<Product> query = (from p in db.Products
+                                          join pv in db.ProductVendors on p.ProductID equals pv.ProductID
+                                          join v in db.Vendors on pv.BusinessEntityID equals v.BusinessEntityID
+                                          where SqlMethods.Like(v.Name, vendorName)
+                                          select p);
 
-            return query;
+            return query.ToList(); ;
         }
 
         public List<string> GetProductNamesByVendorName(string vendorName)
         {
-            List<string> query = (from p in db.Products
-                                  join pv in db.ProductVendors on p.ProductID equals pv.ProductID
-                                  join v in db.Vendors on pv.BusinessEntityID equals v.BusinessEntityID
-                                  where SqlMethods.Like(v.Name, vendorName)
-                                  select p.Name).ToList();
+            IEnumerable<string> query = (from p in db.Products
+                                         join pv in db.ProductVendors on p.ProductID equals pv.ProductID
+                                         join v in db.Vendors on pv.BusinessEntityID equals v.BusinessEntityID
+                                         where SqlMethods.Like(v.Name, vendorName)
+                                         select p.Name);
 
-            return query;
+            return query.ToList(); ;
         }
         public string GetProductVendorByProductName(string productName)
         {
@@ -62,33 +61,30 @@ namespace Task3
 
         public List<Product> GetProductsWithNRecentReviews(int howManyReviews)
         {
-            List<Product> query = (from pr in db.ProductReview
-                    join p in db.Products on pr.ProductID equals p.ProductID
-                    select p)
-                    .Take(howManyReviews).ToList<Product>();
+            IEnumerable<Product> query = (from pr in db.ProductReview
+                                          join p in db.Products on pr.ProductID equals p.ProductID
+                                          select p).Take(howManyReviews);
 
-            return query;
+            return query.ToList(); ;
         }
         public List<Product> GetNRecentlyReviewedProducts(int howManyProducts)
         {
-            List<Product> query = (from p in db.Products
-                    join pr in db.ProductReview on p.ProductID equals pr.ProductID
-                    orderby pr.ReviewDate
-                    select p)
-                    .Take(howManyProducts).ToList<Product>();
+            IEnumerable<Product> query = (from p in db.Products
+                                          join pr in db.ProductReview on p.ProductID equals pr.ProductID
+                                          orderby pr.ReviewDate
+                                          select p).Take(howManyProducts);
 
-            return query;
+            return query.ToList();
         }
         public List<Product> GetNProductsFromCategory(string categoryName, int n)
         {
-            List<Product> query = (from p in db.Products
-                    join ps in db.ProductSubcategory on p.ProductSubcategoryID equals ps.ProductSubcategoryID
-                    join pc in db.ProductCategory on ps.ProductCategoryID equals pc.ProductCategoryID
-                    where pc.Name == categoryName
-                    select p)
-                    .Take(n).ToList();
+            IEnumerable<Product> query = (from p in db.Products
+                                          join ps in db.ProductSubcategory on p.ProductSubcategoryID equals ps.ProductSubcategoryID
+                                          join pc in db.ProductCategory on ps.ProductCategoryID equals pc.ProductCategoryID
+                                          where pc.Name == categoryName
+                                          select p).Take(n);
 
-            return query;
+            return query.ToList(); ;
         }
 
         public double GetTotalStandardCostByCategory(ProductCategory category)
