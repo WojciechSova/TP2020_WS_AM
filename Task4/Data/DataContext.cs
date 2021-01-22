e99deedc916a55e2bf3ef885f723ed14aff115aa
Artur <artur.m99@wp.pl>
2021-01-22 17:57:05 +0100

    Create base of DataContext

---
+++ Task4/Data/DataContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class DataContext : IDataContext<CreditCard>
+    {
+
+        public DataContext()
+        {
+
+        }
+        public void AddItem(CreditCard item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(CreditCard item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CreditCard> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CreditCard GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(CreditCard item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task4/Data/IDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public interface IDataContext<T>
+    {
+        T GetItem(int id);
+        IEnumerable<T> GetAll();
+        void AddItem(T item);
+        void DeleteItem(T item);
+        void UpdateItem(T item);
+    }
+}
