cada504599dc292a5d4009715807d39641078783
Artur <artur.m99@wp.pl>
2021-01-23 16:21:32 +0100

    Add MVVM & implement Model layer

---
+++ Task4/Model/CardModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Logic;
+
+namespace Model
+{
+    public class CardModel : ICreditCard
+    {
+        public CardModel(ICreditCard card)
+        {
+            CreditCardID = card.CreditCardID;
+            CardNumber = card.CardNumber;
+            CardType = card.CardType;
+            ExpMonth = card.ExpMonth;
+            ExpYear = card.ExpYear;
+        }
+        public int CreditCardID { get; set; }
+        public string CardNumber { get; set; }
+        public string CardType { get; set; }
+        public byte ExpMonth { get; set; }
+        public short ExpYear { get; set; }
+    }
+}
---
+++ Task4/Model/CardService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Logic;
+
+namespace Model
+{
+    class CardService
+    {
+        private IService service;
+
+        public CardService(IService service)
+        {
+            this.service = service;
+        }
+
+        public CardModel GetCard(int id)
+        {
+            return (CardModel)service.GetCreditCard(id);
+        }
+
+        public IEnumerable<CardModel> GetAllCreditCards()
+        {
+            return service.GetAllCreditCards().Select(card => new CardModel(card));
+        }
+
+        public void AddCard(CardModel card)
+        {
+            service.AddCreditCard(card);
+        }
+
+        public void DeleteCreditCard(int id)
+        {
+            service.DeleteCreditCard(id);
+        }
+
+        public void UpdateCreditCard(int id, CardModel card)
+        {
+            service.UpdateCreditCard(id, card);
+        }
+    }
+}
