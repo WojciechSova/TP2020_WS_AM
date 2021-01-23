37f7048b9f7af86dfb8150694802b2ca25684b5b
Artur <artur.m99@wp.pl>
2021-01-23 15:07:23 +0100

    Add IDataContext for Service

--- Task4/Logic/Service.cs
+++ Task4/Logic/Service.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
 
 namespace Logic
 {
     class Service : IService
     {
+        private IDataContext<Data.CreditCard> DataContext;
+
+
+
         public void AddCreditCard(ICreditCard creditCard)
         {
             throw new NotImplementedException();
