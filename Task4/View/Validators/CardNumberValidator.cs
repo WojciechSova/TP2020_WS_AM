69e5ae492141c4734c46af01da6779a3356c1747
Artur <artur.m99@wp.pl>
2021-01-24 19:52:52 +0100

    Add validators

---
+++ Task4/View/Validators/CardNumberValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace View.Validators
+{
+    class CardNumberValidator : ValidationRule
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int num = ((string)value).Length;
+            if (num < Min || num > Max)
+            {
+                return new ValidationResult(false, "Number's length must be between " + Min + " and " + Max);
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ Task4/View/Validators/MonthValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace View.Validators
+{
+    class MonthValidator : ValidationRule
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int num = Int32.Parse((string)value);
+            if (num < Min || num > Max)
+            {
+                return new ValidationResult(false, "Not valid month");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
